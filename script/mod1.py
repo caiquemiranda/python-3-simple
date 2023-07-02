3ec530a6a1b4fdcd96d4893261a9b6136886a22d
caiquemiranda <caiquemirandas@gmail.com>
2023-07-02 16:48:03 -0300

    script_c0de

---
+++ script/mod1.py
@@ -0,0 +1,14 @@
+# Este é script/mod1.py :
+
+def ola():
+    print('Olá')
+
+escrito_por = 'Caique'
+
+class saudacao():
+    
+    def manha(self):
+        print('Bom dia!')
+    
+    def noite(self):
+        print('Boa noite!')
