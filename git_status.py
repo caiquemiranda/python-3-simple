ecb8c70ed373ed58e76b2e95d4a2a6e7c41b3f81
caiquemiranda <caiquemirandas@gmail.com>
2023-07-06 20:45:01 -0300

    teste commit

--- git_status.py
+++ git_status.py
@@ -1,11 +1,13 @@
 import subprocess
+from time import sleep
 
 # Executa o comando de commit
 subprocess.run('git add .', shell=True)
+sleep(1)
 
-'''
 # Executa o comando de commit
 subprocess.run('git commit -m "teste commit"', shell=True)
+sleep(1)
 
 # Executa o comando de commit
-subprocess.run('git push origin main', shell=True)'''
+subprocess.run('git push origin main', shell=True)
