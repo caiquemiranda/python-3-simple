d0ec90c1aaec85cb9e61781696a64250ad477359
caiquemiranda <caiquemirandas@gmail.com>
2023-07-06 20:43:03 -0300

    teste commit

---
+++ git_status.py
@@ -0,0 +1,11 @@
+import subprocess
+
+# Executa o comando de commit
+subprocess.run('git add .', shell=True)
+
+'''
+# Executa o comando de commit
+subprocess.run('git commit -m "teste commit"', shell=True)
+
+# Executa o comando de commit
+subprocess.run('git push origin main', shell=True)'''
